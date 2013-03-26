fa00ea4d8a30f50c9fef26fb5f978ee3a11d546a
Steven Voyles <steven.voyles@gmail.com>
2013-03-25 22:51:09 -0500

    Exercise 13, which deals with parameters, unpacking, variables.

---
+++ ex13.rb
@@ -0,0 +1,10 @@
+first, second, third = ARGV
+
+puts "The script is called: #{$0}"
+puts "Your first variable is called: #{first}"
+puts "Your second variable is called: #{second}"
+puts "Your third variable is called: #{third}"
+
+# Extra Credit
+# Try giving fewer than three arguments and see what happens
+# last argument is null
