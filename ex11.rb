7aceb614159ba2c30cbbb0956da1727389e79e47
Steven Voyles <steven.voyles@gmail.com>
2013-03-25 22:39:43 -0500

    exercise 11, which deals with chomp method and gets method

---
+++ ex11.rb
@@ -0,0 +1,16 @@
+print "How old are you? "
+age = gets.chomp()
+print "How tall are you? "
+height = gets.chomp()
+print "How much do you weigh? "
+weight = gets.chomp()
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+# Extra credit
+# Find out what gets and chomp methods do.
+# gets reads the next line from the input stream. See http://www.ruby-doc.org/core-2.0/IO.html#method-i-gets for more.
+# 
+# chomp returns a string with seperator removed. See http://www.ruby-doc.org/core-2.0/String.html#method-i-chomp for more.
+
+
