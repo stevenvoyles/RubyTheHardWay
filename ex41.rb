56c4f20accd38349a0422fe74784b34b3c7904a8
Steven Voyles <steven.voyles@gmail.com>
2013-05-28 23:11:33 -0500

    additional work on ex41

--- ex41.rb
+++ ex41.rb
@@ -11,4 +11,19 @@ def death()
   Process.exit(1)
 end
 
-
+def central_corridor()
+  puts "The Gothans of Planet Percal #25 have invaded your ship and destroyed"
+  puts "your entire crew. You are the last surviving member and your last"
+  puts "mission is to get the neutron destruct bomb from the Weapons Armory,"
+  puts "put it in the bridge, and blow the ship up after getting into an "
+  puts "escape pod."
+  puts "\n"
+  puts "You're running down the central corridor to the Weapons Armory when"
+  puts "a Gothon jumps out, red scaly skin, dark grimy teeth, and evil clown costume"
+  puts "flowing around his hate filled body. He's blocking the door to the"
+  puts "Armory and about to pull a weapon to blast you."
+  
+  prompt()
+  action = gets.chomp()
+  
+  
